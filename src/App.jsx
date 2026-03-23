97cc9cfb50da6858e44929b8c406af32e3ca87d2
Arjunslab <166507445+Arjunslab@users.noreply.github.com>
2026-03-23 23:03:08 +0530

    Add notfound

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Chatbot from './components/Chatbot'
-import Navbar from './components/Navbar'
+import NotFound from './components/Notfound'
 
-function App() {
+function Home() {
   return (
     <main className="bg-[#050510] min-h-screen">
       <Navbar />
@@ -13,6 +16,17 @@ function App() {
       <Projects />
       <Chatbot />
     </main>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
